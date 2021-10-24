fafe7553c0148856aaab333379466e412f0db2eb
gost214 <81175559+gost214@users.noreply.github.com>
2021-10-25 00:33:08 +0300

    Add files via upload

---
+++ Pract8/IWaitList.java
@@ -0,0 +1,10 @@
+package Pract8;
+import java.util.Collection;
+
+public interface IWaitList<E> {
+    public void add(E element);
+    public E remove();
+    public boolean contains(E element);
+    public boolean containsAll(Collection<E> c);
+    public boolean isEmpty();
+}
